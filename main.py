5ccade4d4e200499e939e8e850d95d1cca3238eb
HuiXin681 <s10223047@connect.np.edu.sg>
2022-11-01 00:00:45 +0800

    Test case 1 & 4 Completed

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    chrmDriver = webdriver.Chrome()
+    chrmDriver.get("https://localhost:44323/Product/ProductDetail/2")
+
+    # Find elements in login page & enter test case
+
+    # Test Case 1 : Login 
+    username = chrmDriver.find_element(By.ID, 'uname')
+    username.send_keys('ProductManager')
+    password = chrmDriver.find_element(By.ID, 'upass')
+    password.send_keys('passProduct')
+    submitBtn = chrmDriver.find_element(By.ID, 'submitBtn')
+    submitBtn.send_keys(Keys.ENTER)
+
+    # Test Case 4 : Obsolete button
+    obsoleteBtn = chrmDriver.find_element(By.ID, 'obsoleteBtn')
+    obsoleteBtn.send_keys(Keys.ENTER)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
